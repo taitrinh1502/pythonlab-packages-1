278ced275f39f3e336c87f59e35b3bdec682d4ef
Minh Tai Trinh <61240170+taitrinh1502@users.noreply.github.com>
2021-10-22 03:08:46 +0000

    22102021 10am

--- progs/main2.py
+++ progs/main2.py
@@ -1,6 +1,28 @@
 from sys import path
 
-path.append('..\\packages')
+#path.append('../packages/Extrapack.zip')
 
-import extra.iota
-print(extra.iota.FunI())
+
+# import extra.iota
+# from extra.iota import FunI
+# print(extra.iota.FunI())
+# print(FunI())
+
+
+# import extra.good.best.sigma
+# from extra.good.best.sigma import FunS
+
+# print(extra.good.best.sigma.FunS())
+# print(FunS())
+
+
+# import extra.good.best.sigma as sig
+# import extra.good.alpha as alp
+# from extra.iota import FunI
+# from extra.good.beta import FunB
+
+# print(sig.FunS())
+# print(alp.FunA())
+# print(FunI())
+# print(FunB())
+
